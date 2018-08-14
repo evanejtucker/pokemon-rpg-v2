526c6447517e4c16c3466c64b899c3dd7034dde7
Evan Tucker <eej.tucker@gmail.com>
2018-08-14 08:22:05 -0600

    began working on autocomplete

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -2,42 +2,67 @@
 module.exports = (app, request)=> {
     // get pokemon by name or id
     app.get('/api/pokemon/name/:id', (req, res, next)=> {
-        console.log(req.params);
+        console.log("parameter: " + req.params);
         var pokemon;
         var queryUrl = 'https://pokeapi.co/api/v2/pokemon/' + req.params.id + "/";
         console.log(queryUrl);
-        console.log('waiting ...')
         request({
             uri: queryUrl
         }, function(error, response, body) {
+            console.log('requesting...');
             if (!error && response.statusCode === 200) {
                 pokemon = JSON.parse(body);
                 console.log('done');
                 res.send(pokemon)
-            }
+            } else {
+                console.log('something went wrong');
+                // if no results are found
+                res.send('error');
+            }      
+        });
+    });
+
+    app.get('/api/pokemon/all/', (req, res, next)=> {
+        console.log("parameter: " + req.params);
+        var pokemon;
+        var queryUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=949';
+        console.log(queryUrl);
+        request({
+            uri: queryUrl
+        }, function(error, response, body) {
+            console.log('requesting...');
+            if (!error && response.statusCode === 200) {
+                pokemon = JSON.parse(body);
+                console.log('done');
+                res.send(pokemon)
+            } else {
+                console.log('something went wrong');
+                // if no results are found
+                res.send('error');
+            }      
         });
     });
 
     // get pokemon move by name or id
     app.get('/api/pokemon/move/:id', (req, res, next)=> {
-        console.log(req.params);
+        console.log("parameter: " + req.params);
         var move;
         var queryUrl = 'https://pokeapi.co/api/v2/pokemon/' + req.params.id + "/";
         console.log(queryUrl);
-        // console.log('waiting ...')
         request({
             uri: queryUrl
         }, function(error, response, body) {
             console.log('requesting...');
-            console.log('error ' + error);
-            console.log('response ' + response);
-            console.log('body ' + body);
             if (!error && response.statusCode === 200) {
                 move = JSON.parse(body);
                 console.log('done');
                 res.send(move)
+            } else {
+                console.log('something went wrong');
+                // if no results are found
+                res.send('error');
             }
-            res.send('well shit')
+            
         });
     });
 
