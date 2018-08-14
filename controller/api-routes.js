75374b20722e69fdda0d0dcd1d35f5a8efca5ff3
Evan Tucker <eej.tucker@gmail.com>
2018-08-13 19:41:30 -0600

    worked on api routes

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -1,6 +1,7 @@
 
 module.exports = (app, request)=> {
-    app.get('/api/pokemon/:id', (req, res, next)=> {
+    // get pokemon by name or id
+    app.get('/api/pokemon/name/:id', (req, res, next)=> {
         console.log(req.params);
         var pokemon;
         var queryUrl = 'https://pokeapi.co/api/v2/pokemon/' + req.params.id + "/";
@@ -11,13 +12,36 @@ module.exports = (app, request)=> {
         }, function(error, response, body) {
             if (!error && response.statusCode === 200) {
                 pokemon = JSON.parse(body);
-                // res.json(pokemon); 
-                res.render('pokedex', {pokemon: pokemon});
-                // console.log(__dirname)
                 console.log('done');
+                res.send(pokemon)
             }
         });
     });
+
+    // get pokemon move by name or id
+    app.get('/api/pokemon/move/:id', (req, res, next)=> {
+        console.log(req.params);
+        var move;
+        var queryUrl = 'https://pokeapi.co/api/v2/pokemon/' + req.params.id + "/";
+        console.log(queryUrl);
+        // console.log('waiting ...')
+        request({
+            uri: queryUrl
+        }, function(error, response, body) {
+            console.log('requesting...');
+            console.log('error ' + error);
+            console.log('response ' + response);
+            console.log('body ' + body);
+            if (!error && response.statusCode === 200) {
+                move = JSON.parse(body);
+                console.log('done');
+                res.send(move)
+            }
+            res.send('well shit')
+        });
+    });
+
+    
 };
 
 
--- public/scripts/pokedex.js
+++ public/scripts/pokedex.js
@@ -7,11 +7,29 @@ $(document).ready(function() {
 // Functions
 // -----------------------------------------------------------------------------------
 
+var searchPokemon = function(searchTerm) {
+    var queryUrl = '/api/pokemon/name/' + searchTerm + '/';
+    console.log(queryUrl)
+    $.get({
+        url: queryUrl,
+    }).done(function(data) {
+        console.log('got something back');
+        console.log(data);
+    }).fail(function(err){
+        if (err) {
+            console.log('something went wrong');
+        }
+    });
+}
 
 // Main Process
 // -----------------------------------------------------------------------------------
     
-
+$('#pokemonSubmit').on('click', function() {
+    event.preventDefault();
+    var searchTerm = $('#pokemonSearch').val();
+    searchPokemon(searchTerm);
+});
 
 // $(document).ready closing tag
 });
